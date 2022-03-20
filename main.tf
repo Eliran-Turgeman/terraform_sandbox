967555cc03aad23dc55d1e3acbe3bf37813ce477
Eliran Turgeman <elturgeman@paloaltonetworks.com>
2022-03-20 14:10:38 +0200

    added rds

--- main.tf
+++ main.tf
@@ -130,5 +130,16 @@ resource "aws_instance" "ec2-eliran" {
   }
 }
 
-#trial
+
+resource "aws_db_instance" "mysql_eliran_db" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "eliran"
+  password             = "eliran"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
 
